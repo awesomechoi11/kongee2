e588ada3f0c9d33d6b91e9cb117c3884793add53
awesomechoi11 <brandonchoi2016@gmail.com>
2022-03-06 16:28:10 -0800

    theatre js hooks

--- components/threestuff/NameBanner/NameBanner.js
+++ components/threestuff/NameBanner/NameBanner.js
@@ -1,5 +1,3 @@
-import { useLoader } from '@react-three/fiber';
-import { TextureLoader } from 'three/src/loaders/TextureLoader';
 import fragment from './fragment.glsl';
 import { useEffect, useState } from 'react';
 import { useTexture } from '@react-three/drei';
@@ -7,16 +5,16 @@ import { useTexture } from '@react-three/drei';
 export default function NameBanner() {
   // const texture = useLoader(TextureLoader, '/api/images/nameBanner.jpg');
 
-  const texture = useTexture('/api/images/nameBanner.jpg');
+  const texture = useTexture('/api/images/Frame 209.jpg');
 
   const [ready, setReady] = useState(false);
   useEffect(() => {
     setReady(true);
   }, []);
   if (!ready) return null;
-
+  // 1128 x 186
   return (
-    <mesh>
+    <mesh position={[2, 1, 3]}>
       <planeGeometry args={[10, 10]} />
       <shaderMaterial
         uniforms={{
